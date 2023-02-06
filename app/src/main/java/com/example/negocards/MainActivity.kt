57d2ebac08e0174a8f8c83cf56e7f864693166b7
M1KoTiK-Notebook <temhbz@mail.ru>
2023-02-07 02:56:34 +0700

    Переходы между экранами через нижнее меню

--- app/src/main/java/com/example/negocards/MainActivity.kt
+++ app/src/main/java/com/example/negocards/MainActivity.kt
@@ -10,25 +10,24 @@ import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.negocards.databinding.ActivityMainBinding
-import me.ibrahimsn.lib.SmoothBottomBar
 
 class MainActivity : AppCompatActivity() {
     private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-       // navController = findNavController(R.id.)
+        navController = findNavController(R.id.fragmentContainerView)
 //        setupActionBarWithNavController(navController)
         setupSmoothBottomMenu()
-        setContentView(R.layout.activity_main)
     }
     private fun setupSmoothBottomMenu() {
         val popupMenu = PopupMenu(this, null)
         popupMenu.inflate(R.menu.bottom_navigation_bar)
         val menu = popupMenu.menu
-        //binding.bottomBar.setupWithNavController(menu, navController)
+        binding.bottomBar.setupWithNavController(menu, navController)
        // binding.bottomBar.setupWithNavController( navController)
     }
 }
