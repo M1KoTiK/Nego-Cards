318e418a97dd3fa1e18ac227afbf8ca883a442c7
M1KoTiK-Notebook <temhbz@mail.ru>
2023-02-06 12:57:41 +0700

    Включил меню навигации в проект

--- app/src/main/java/com/example/negocards/MainActivity.kt
+++ app/src/main/java/com/example/negocards/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.negocards
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-//Ура репоизиторий больше не сломан
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.PopupMenu
+import android.widget.Toast
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.negocards.databinding.ActivityMainBinding
+import me.ibrahimsn.lib.SmoothBottomBar
+
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+       // navController = findNavController(R.id.)
+//        setupActionBarWithNavController(navController)
+        setupSmoothBottomMenu()
         setContentView(R.layout.activity_main)
+    }
+    private fun setupSmoothBottomMenu() {
+        val popupMenu = PopupMenu(this, null)
+        popupMenu.inflate(R.menu.bottom_navigation_bar)
+        val menu = popupMenu.menu
+        //binding.bottomBar.setupWithNavController(menu, navController)
+       // binding.bottomBar.setupWithNavController( navController)
     }
 }
---
+++ app/src/main/java/com/example/negocards/fragments/pages/BlankFragment.kt
@@ -0,0 +1,16 @@
+package com.example.negocards.fragments.pages
+
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.example.negocards.R
+
+class FirstFragment : Fragment(R.layout.blank_fragment) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
