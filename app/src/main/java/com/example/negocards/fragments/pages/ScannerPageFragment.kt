76719b3ce9c38efe7c2b0286e5b1c46c292b9719
M1KoTiK-Notebook <temhbz@mail.ru>
2023-03-06 04:02:13 +0700

    Сканер теперь работает

--- app/src/main/java/com/example/negocards/fragments/pages/ScannerPageFragment.kt
+++ app/src/main/java/com/example/negocards/fragments/pages/ScannerPageFragment.kt
@@ -1,22 +1,45 @@
 package com.example.negocards.fragments.pages
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.CameraSelector.LENS_FACING_BACK
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.camera.view.PreviewView
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import com.example.negocards.R
-import com.example.negocards.databinding.FragmentMainPageBinding
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.negocards.CameraXViewModel
 import com.example.negocards.databinding.FragmentScannerPageBinding
-import com.example.negocards.model.QR.QRCreator
+import com.google.mlkit.vision.barcode.Barcode
+import com.google.mlkit.vision.barcode.BarcodeScanner
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.common.InputImage
+import java.util.concurrent.Executors
+
 
 class ScannerPageFragment : Fragment() {
     private var binding: FragmentScannerPageBinding? = null
+
+    private var previewView: PreviewView? = null
+    private var cameraProvider: ProcessCameraProvider? = null
+    private var cameraSelector: CameraSelector? = null
+    private var lensFacing = CameraSelector.LENS_FACING_BACK
+    private var previewUseCase: Preview? = null
+    private var analysisUseCase: ImageAnalysis? = null
+
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -29,15 +52,148 @@ class ScannerPageFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if (isCameraPermissionGranted()) {
-            // startCamera
-        } else {
-            ActivityCompat.requestPermissions(
-                requireActivity(),
-                arrayOf(Manifest.permission.CAMERA),
-                PERMISSION_CAMERA_REQUEST
-            )
+        setupCamera()
+    }
+    private fun setupCamera() {
+        previewView = binding?.previewView
+        cameraSelector = CameraSelector.Builder().requireLensFacing(lensFacing).build()
+        ViewModelProvider(
+            this, ViewModelProvider.AndroidViewModelFactory.getInstance(requireActivity().application)
+        ).get(CameraXViewModel::class.java)
+            .processCameraProvider
+            .observe(requireActivity()) { provider: ProcessCameraProvider? ->
+                cameraProvider = provider
+                if (isCameraPermissionGranted()) {
+                    bindCameraUseCases()
+                } else {
+                    ActivityCompat.requestPermissions(
+                        requireActivity(),
+                        arrayOf(Manifest.permission.CAMERA),
+                        PERMISSION_CAMERA_REQUEST
+                    )
+                }
+            }
+    }
+
+    private fun bindCameraUseCases() {
+        bindPreviewUseCase()
+        bindAnalyseUseCase()
+    }
+
+    private fun bindPreviewUseCase() {
+        if (cameraProvider == null) {
+            return
         }
+        if (previewUseCase != null) {
+            cameraProvider!!.unbind(previewUseCase)
+        }
+
+        previewUseCase = Preview.Builder()
+            .setTargetRotation(previewView!!.display.rotation)
+            .build()
+        previewUseCase!!.setSurfaceProvider(previewView!!.surfaceProvider)
+
+        try {
+
+            cameraProvider!!.bindToLifecycle(
+                this,
+                cameraSelector!!,
+                previewUseCase
+            )
+        } catch (illegalStateException: IllegalStateException) {
+            Log.e(TAG, illegalStateException.message ?: "IllegalStateException")
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            Log.e(TAG, illegalArgumentException.message ?: "IllegalArgumentException")
+        }
+    }
+
+    private fun bindAnalyseUseCase() {
+        val options = BarcodeScannerOptions.Builder()
+            .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS).build()
+
+        val barcodeScanner: BarcodeScanner = BarcodeScanning.getClient(options)
+
+        if (cameraProvider == null) {
+            return
+        }
+        if (analysisUseCase != null) {
+            cameraProvider!!.unbind(analysisUseCase)
+        }
+
+        analysisUseCase = ImageAnalysis.Builder()
+            .setTargetRotation(previewView!!.display.rotation)
+            .build()
+
+        // Initialize our background executor
+        val cameraExecutor = Executors.newSingleThreadExecutor()
+
+        analysisUseCase?.setAnalyzer(
+            cameraExecutor,
+            ImageAnalysis.Analyzer { imageProxy ->
+                processImageProxy(barcodeScanner, imageProxy)
+            }
+        )
+
+        try {
+            cameraProvider!!.bindToLifecycle(
+                /* lifecycleOwner= */this,
+                cameraSelector!!,
+                analysisUseCase
+            )
+        } catch (illegalStateException: IllegalStateException) {
+            Log.e(TAG, illegalStateException.message ?: "IllegalStateException")
+        } catch (illegalArgumentException: IllegalArgumentException) {
+            Log.e(TAG, illegalArgumentException.message ?: "IllegalArgumentException")
+        }
+    }
+
+    @SuppressLint("UnsafeOptInUsageError")
+    private fun processImageProxy(
+        barcodeScanner: BarcodeScanner,
+        imageProxy: ImageProxy
+    ) {
+        val inputImage =
+            InputImage.fromMediaImage(imageProxy.image!!, imageProxy.imageInfo.rotationDegrees)
+
+        barcodeScanner.process(inputImage)
+            .addOnSuccessListener { barcodes ->
+                barcodes.forEach { barcode ->
+                    val bounds = barcode.boundingBox
+                    val corners = barcode.cornerPoints
+
+                    val rawValue = barcode.rawValue
+
+                    binding?.tvScannedData?.text = barcode.rawValue
+
+                    val valueType = barcode.valueType
+                    // See API reference for complete list of supported types
+                    when (valueType) {
+                        Barcode.TYPE_WIFI -> {
+                            val ssid = barcode.wifi!!.ssid
+                            val password = barcode.wifi!!.password
+                            val type = barcode.wifi!!.encryptionType
+                            binding?.tvScannedData?.text =
+                                "ssid: " + ssid + "\npassword: " + password + "\ntype: " + type
+                        }
+                        Barcode.TYPE_URL -> {
+                            val title = barcode.url!!.title
+                            val url = barcode.url!!.url
+
+                            binding?.tvScannedData?.text = "Title: " + title + "\nURL: " + url
+                        }
+                    }
+                }
+            }
+            .addOnFailureListener {
+                Log.e(TAG, it.message ?: it.toString())
+            }
+            .addOnCompleteListener {
+                // When the image is from CameraX analysis use case, must call image.close() on received
+                // images when finished using them. Otherwise, new images may not be received or the camera
+                // may stall.
+                imageProxy.close()
+
+            }
     }
 
     override fun onRequestPermissionsResult(
@@ -61,6 +217,8 @@ class ScannerPageFragment : Fragment() {
             Manifest.permission.CAMERA
         ) == PackageManager.PERMISSION_GRANTED
     }
+
+
 
     companion object {
         private val TAG = ScannerPageFragment::class.java.simpleName
