59bf5b4d5a580655f7c62b5d6b33dd6b33dcb2f3
M1KoTiK-Notebook <temhbz@mail.ru>
2023-10-02 18:21:51 +0700

    Жопа я неправильно сделал парсер

--- app/src/main/java/m1k/kotik/negocards/data/serialization/serializationObject/TestSerializeObject.kt
+++ app/src/main/java/m1k/kotik/negocards/data/serialization/serializationObject/TestSerializeObject.kt
@@ -3,7 +3,7 @@ package m1k.kotik.negocards.data.serialization.serializationObject
 class TestSerializeObject: ISerializationObject {
     override val key: String = "testObject"
     @SeriаlizationMember("int")
-    var integer  = "1234"
+    var integer  = 1234
 
     @SeriаlizationMember("text")
     var text = "1256"
